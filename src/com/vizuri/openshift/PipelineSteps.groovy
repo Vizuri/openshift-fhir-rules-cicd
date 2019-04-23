827085ca4947facb81119b95bcca93d17d1a3ac5
Kent Eudy <keudy@vizuri.com>
2019-04-23 15:26:42 -0400

    updates

--- src/com/vizuri/openshift/PipelineSteps.groovy
+++ src/com/vizuri/openshift/PipelineSteps.groovy
@@ -12,7 +12,7 @@ def setEnv(pipelineParams) {
 	//env.NEXUS_URL = "http://nexus-${pipelineParams.registryUsername}-cicd.${pipelineParams.ocpAppSuffix}"
 }
 
-def checkout() {
+def checkout(projectFolder = ".") {
 	echo "In checkout"
 	stage('Checkout') {
 
